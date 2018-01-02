5aa0c07bf35d85c65aeb8f2fed79785610033459
taihsinhsu <31632610+taihsinhsu@users.noreply.github.com>
2018-01-02 00:07:48 -0500

    Update pc.js

--- pc.js
+++ pc.js
@@ -1,6 +1,6 @@
     var margin = {top: 30, right: 10, bottom: 10, left: 10},
-        width = 800 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        width = 500 - margin.left - margin.right,
+        height = 700 - margin.top - margin.bottom;
 
     var x = d3.scale.ordinal().rangePoints([0, width], 1),
         y = {},
